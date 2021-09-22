87eb2c59caef02814218b13f828c443ee391b177
gabrielfribeiro <gabrielfribeiro44@gmail.com>
2021-09-22 15:40:29 -0300

    feat: added github template and dependabot

---
+++ styles/teste.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Teste = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 10;
+  height: 10;
+  margin-top: 0;
+`
