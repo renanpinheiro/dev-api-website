d3c174026b7b969815eec31486427aa2ad252619
gabrielfribeiro <gabrielfribeiro44@gmail.com>
2021-09-23 13:53:52 -0300

    fix: fixed some imports

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
 
-import Head from 'next/head'
-import Hero from 'components/Hero'
-
-const Home: React.FC = () => {
+const Home = () => {
   return (
     <>
-      <Head>
-        <title>Home</title>
-      </Head>
-
-      <main>
-        <Hero />
-      </main>
+      <div>HOME</div>
     </>
   )
 }
