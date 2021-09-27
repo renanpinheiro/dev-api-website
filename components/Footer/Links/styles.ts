6e3c8ed01c4451f4ead0bc01402498f865c751f5
marciock <marciockalves@gmail.com>
2021-09-27 08:33:31 -0300

    fix: fixed slint lib into css

--- components/Footer/Links/styles.ts
+++ components/Footer/Links/styles.ts
@@ -2,23 +2,23 @@ import styled from 'styled-components'
 import { theme } from '../../../styles/theme'
 
 export const ContainerLink = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
   width: 60%;
   height: 100%;
   padding: 10px;
-  display: flex;
-  justify-content: flex-start;
-  flex-direction: column;
 
   > a {
+    padding-bottom: 20px;
     margin: 0;
     margin-bottom: 5px;
-    color: ${props => props.theme.colors.white};
     font-size: 1.25rem;
+    font-size: 20px;
+    color: ${props => props.theme.colors.white};
+    text-decoration: none;
     cursor: pointer;
     transition: all 0.2s;
-    text-decoration: none;
-    font-size: 20px;
-    padding-bottom: 20px;
 
     &:hover {
       color: ${props => props.theme.colors.secondary};
@@ -31,10 +31,10 @@ export const ContainerLink = styled.div`
   }
 `
 export const TitleLink = styled.p`
-  color: ${props => props.theme.colors.secondary};
-  font-weight: 700;
-  font-size: 20px;
   padding-bottom: 26px;
+  font-size: 20px;
+  font-weight: 700;
+  color: ${props => props.theme.colors.secondary};
 
   @media only screen and (max-width: 828px) {
     width: auto;
--- components/Footer/NewsletterForm/styles.ts
+++ components/Footer/NewsletterForm/styles.ts
@@ -2,13 +2,12 @@ import styled from 'styled-components'
 import { theme } from '../../../styles/theme'
 
 export const ContainerForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  max-width: 450px;
   margin-top: 20px;
   margin-bottom: 65px;
-  max-width: 450px;
-
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
 
   @media only screen and (max-width: 828px) {
     width: auto;
@@ -27,69 +26,69 @@ export const ContainerForm = styled.div`
   }
 `
 export const InputGroup = styled.div`
-  width: 100%;
-  height: 60px;
-  margin: 10px;
   display: flex;
   flex-direction: row;
   align-items: center;
+  width: 100%;
+  height: 60px;
+  margin: 10px;
 `
 export const InputNewsletter = styled.input`
   width: 100%;
   height: 100%;
   padding: 0.75rem 0.75rem;
 
-  color: ${theme.colors.gray[800]};
-  border: 1px solid ${theme.colors.gray[500]};
-  border-right: 0;
-  border-radius: 5px 0 0 5px;
-
-  background-color: white;
-  background-clip: padding-box;
-
   font-size: 1rem;
   font-weight: 400;
   line-height: 1.5;
+
+  color: ${theme.colors.gray[800]};
+
+  background-color: white;
+  background-clip: padding-box;
+  border: 1px solid ${theme.colors.gray[500]};
+  border-right: 0;
+  border-radius: 5px 0 0 5px;
+  outline: none;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+    -webkit-box-shadow 0.15s ease-in-out;
 
   &:focus {
     border: 1px solid ${theme.colors.green[100]};
     border-right: 0;
   }
-  outline: none;
-  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
-    -webkit-box-shadow 0.15s ease-in-out;
 `
 export const ButtonNewsLetter = styled.button`
+  position: relative;
+
+  left: -4px;
+  z-index: 2;
   width: auto;
   height: 100%;
   padding: 1rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
 
-  left: -4px;
-  position: relative;
-  z-index: 2;
-  cursor: pointer;
+  color: white;
 
   text-align: center;
   vertical-align: middle;
-  font-weight: 400;
-  font-size: 1rem;
-
-  color: white;
+  cursor: pointer;
+  user-select: none;
   background-color: ${theme.colors.green[100]};
-
-  &:active {
-    background-color: ${theme.colors.green[200]};
-  }
-
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
-    -webkit-box-shadow 0.15s ease-in-out;
 
   border: 1px solid ${theme.colors.gray[500]};
   border-left: 0;
 
   border-radius: 0 5px 5px 0;
-  user-select: none;
+
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+    -webkit-box-shadow 0.15s ease-in-out;
+
+  &:active {
+    background-color: ${theme.colors.green[200]};
+  }
 
   @media only screen and (max-width: 996px) {
     font-size: 1.25rem;
