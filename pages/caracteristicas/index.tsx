05d76cc232b797beca49e497e1c6adbb9f67c7d0
gabrielfribeiro <gabrielfribeiro44@gmail.com>
2021-09-30 10:18:30 -0300

    feat: fixed stylelint

--- pages/caracteristicas/index.tsx
+++ pages/caracteristicas/index.tsx
@@ -52,6 +52,34 @@ const Characteristics = () => {
             )
           })}
         </S.IntegrationContainer>
+        <S.ProfessionalServiceContainer>
+          <S.ImageContainer>
+            <img
+              src="/logo/professional-service.svg"
+              alt="professional-service"
+            />
+          </S.ImageContainer>
+          <S.ProfessionalServiceText>
+            <S.Pipe />
+            <h2>
+              <span>Professional Services,</span> um time de especialistas
+              dedicado ao seu negócio
+            </h2>
+            <p>
+              A DevApi é composta por profissionais especializados no
+              desenvolvimento de integrações e APIs, de forma a maximizar os
+              resultados de nossos parceiros e clientes.
+            </p>
+            <S.CheckContainer>
+              <img src="/icons/circle-check.svg" alt="check" />
+              <p>
+                Uma equipe de alta performance ao serviço do sucesso da sua
+                empresa!
+              </p>
+            </S.CheckContainer>
+            <Button size="default" text="Quero um orçamento!" type="default" />
+          </S.ProfessionalServiceText>
+        </S.ProfessionalServiceContainer>
       </S.Container>
     </>
   )
