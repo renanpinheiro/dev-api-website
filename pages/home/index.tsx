8500015ab6e3104994d0eac108e011e9f9a21531
Gasparotto <matheus.gasparotto@devapi.com.br>
2021-10-01 10:41:35 -0300

    feat: create call to action button and text

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -6,7 +6,7 @@ import * as S from '../../styles/home'
 import dynamic from 'next/dynamic'
 import QuoteCarousel from '../../components/QuoteCarousel'
 import { quotes } from '../../constants/quotes'
-
+import { Button } from '../../components/Button'
 const CarrouselWithOutSSR = dynamic(
   () => import('../../components/Carrousel'),
   {
@@ -50,6 +50,21 @@ const Home = () => {
             }
           />
         )}
+        <S.ActionContainer>
+          <S.ActionTextContainer>
+            <S.Pipe />
+            <h3>
+              Quer testar a DevApi <span>gratuitamente?</span>
+            </h3>
+          </S.ActionTextContainer>
+          <Button
+            size={'large'}
+            text={
+              width < 500 ? 'Entrar em contato' : 'Converse com um especialista'
+            }
+            type={'default'}
+          />
+        </S.ActionContainer>
       </S.CostumersCarrouselContainer>
       <S.QuoteContainer>
         <QuoteCarousel quotes={quotes} />
--- styles/home.ts
+++ styles/home.ts
@@ -14,6 +14,7 @@ export const CarrouselTitle = styled.h2`
     font-weight: 700;
   }
 `
+
 export const CostumersCarrouselContainer = styled.div`
   width: 100%;
   padding: 0 8vw;
@@ -26,4 +27,50 @@ export const Container = styled.div`
 
 export const QuoteContainer = styled.div`
   padding: 20px 0;
+`
+export const Pipe = styled.div`
+  width: 10vh;
+  height: 3px;
+  margin-bottom: 10px;
+
+  background-color: ${theme.colors.secondary};
+
+  @media (max-width: 576px) {
+    width: 8vh;
+  }
+`
+
+export const ActionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 5vh 8vw 0 8vw;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+`
+
+export const ActionTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 35ch;
+  margin-right: 3vw;
+
+  @media (max-width: 1024px) {
+    align-items: center;
+    width: 100%;
+    padding-bottom: 1vh;
+    text-align: center;
+  }
+
+  h3 {
+    font-size: 2.2rem;
+  }
+
+  span {
+    font-weight: 700;
+  }
 `
