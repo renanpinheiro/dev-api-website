576948e5b6f2251e1f0441a70aa6c5b7856c42a2
William Hoffmann <william@devapi.com.br>
2021-10-02 21:51:45 -0300

    fix: adjusting hero component interface

--- components/Hero/Hero.interface.ts
+++ components/Hero/Hero.interface.ts
@@ -1,3 +1,11 @@
+import { ReactNode } from 'react'
+
+export interface IHeroProps {
+  minHeight?: number
+  maxHeight?: number
+  children: ReactNode
+}
+
 export interface IContainerStyle {
   minHeight?: number
   maxHeight?: number
