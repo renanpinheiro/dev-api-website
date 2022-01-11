01f9a9c840528c4a9ad5c9f81e4ea1e9b99d31b9
gabrielfribeiro <gabrielfribeiro44@gmail.com>
2022-01-11 11:17:52 -0300

    fix: fixed home responsive

--- components/Header/menuOptions.ts
+++ components/Header/menuOptions.ts
@@ -3,9 +3,12 @@ import { INavLinks } from './Header.interface'
 
 export const handleNavLink = (route: string): INavLinks[] => {
   const handledNavLinks = navLinks.map(link => {
+    console.log(link)
     return {
       name: link.name,
-      isActive: link.dropDown && link.dropDown.some(item => item.url === route),
+      isActive:
+        (link.dropDown && link.dropDown.some(item => item.url === route)) ||
+        link.url === route,
       dropDown:
         link.dropDown &&
         link.dropDown.map(item => {
