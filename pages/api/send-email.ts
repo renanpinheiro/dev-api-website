19111ae30dfbfe24909ceab6a96a424f6a28d835
Lucas Marchiori <sinonsrt@hotmail.com>
2022-02-22 09:03:42 -0300

    fix: change partner e-mail form

---
+++ pages/api/send-email.ts
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer'
+
+export default async (req, res) => {
+  try {
+    const { fullName, email, phone, company, role } = req.body
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.NEXT_PUBLIC_SMTP,
+      port: Number(process.env.NEXT_PUBLIC_PORT),
+      secure: true,
+      auth: {
+        user: process.env.NEXT_PUBLIC_USER,
+        pass: process.env.NEXT_PUBLIC_PASS,
+      },
+    })
+
+    await transporter.sendMail({
+      from: process.env.NEXT_PUBLIC_USER,
+      to: 'michel.carnevali@devapi.com.br, luana@devapi.com.br',
+      subject: `Solicitação de Parceria de Integração - ${company}`,
+      text: `Nome: ${fullName} \n E-mail: ${email} \n Telefone: ${phone} \n Empresa: ${company} \n Cargo: ${role}`,
+    })
+
+    return res.status(200).json({
+      message: 'E-mail enviado com sucesso! Em breve entraremos em contato.',
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Erro ao enviar e-mail.',
+    })
+  }
+}
