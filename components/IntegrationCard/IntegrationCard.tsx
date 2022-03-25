5691ce9adb062ee34064f2b9343128f9433b91c5
gabrielfribeiro <gabrielfribeiro44@gmail.com>
2022-03-25 14:40:29 -0300

    feat: created animation

--- components/IntegrationCard/IntegrationCard.tsx
+++ components/IntegrationCard/IntegrationCard.tsx
@@ -4,22 +4,22 @@ import { IIntegrationCard } from './IntegrationCard.interface'
 import * as S from './IntegrationCard.style'
 
 const IntegrationCard = ({ imageLeft, imageRight }: IIntegrationCard) => {
+  const balls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
   return (
     <S.Container>
       <S.ImageContainer>
         <S.ConnectorImage src={imageLeft}></S.ConnectorImage>
       </S.ImageContainer>
-      <S.ConnectorContainer>
-        <S.BallContainer>
-          <S.LittleBallImage src="/images/littleball-left.svg"></S.LittleBallImage>
-        </S.BallContainer>
+      <S.BallContainer>
+        {balls.map(() => {
+          return <S.Balls></S.Balls>
+        })}
+
         <S.ContainerDevApiLogo>
           <S.DevApiLogoImage src="/images/mini-devapi.svg"></S.DevApiLogoImage>
         </S.ContainerDevApiLogo>
-        <S.BallContainer>
-          <S.LittleBallImage src="/images/littleball-right.svg"></S.LittleBallImage>
-        </S.BallContainer>
-      </S.ConnectorContainer>
+      </S.BallContainer>
       <S.ImageContainer>
         <S.ConnectorImage src={imageRight}></S.ConnectorImage>
       </S.ImageContainer>
